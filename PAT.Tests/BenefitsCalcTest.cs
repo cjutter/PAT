02f070abbab2b1a80f84205865eac92e466376b7
Charles Utter <cutter@utilant.com>
2017-11-11 20:39:34 -0500

    Init

---
+++ PAT.Tests/BenefitsCalcTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PAT.Tests
+{
+    [TestClass]
+    public class BenefitsCalcTest
+    {
+        [TestMethod]
+        public void CalculateCosts()
+        {
+            var benefitsCalc = IocContainer.BenefitsCalculator;
+        }
+    }
+
+    public class BenefitsCalculator : IPayrollCalculator
+    { 
+        public ICalculationResult Calculate(IEmployee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IPayrollCalculator
+    {
+        ICalculationResult Calculate(IEmployee employee);
+    }
+
+    public interface ICalculationResult
+    {
+        
+    }
+
+    public static class IocContainer
+    {
+        private static IPayrollCalculator _calc;
+
+        public static IPayrollCalculator GetBenefitsCalculator(IEmployee employee)
+        {
+            if (_calc != null) return _calc;
+            _calc = new BenefitsCalculator();
+            return _calc;
+        }
+    }
+}
+
+    public interface IEmployee
+    {
+        
+    }
+}
---
+++ PAT.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PAT.UI.MvcApplication" Language="C#" %>
