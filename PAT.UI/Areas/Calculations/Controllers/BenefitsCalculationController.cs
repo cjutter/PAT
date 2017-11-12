cfeece33290cfabde7c95d47e03614c11e2a4377
Charles Utter <cutter@utilant.com>
2017-11-12 16:01:52 -0500

    client side validation

---
+++ PAT.UI/Areas/Calculations/Controllers/BenefitsCalculationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PAT.UI.Areas.Calculations.Controllers
+{
+    public class BenefitsCalculationController : Controller
+    {
+        // GET: Calculations/BenefitsCalculation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PAT.UI/Areas/Calculations/Models/EmployeeBenefitsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PAT.UI.Areas.Calculations.Models
+{
+    public class EmployeeBenefitsViewModel
+    {
+        [DisplayName("Employee ID")]
+        [Required]
+        [Range(0,50000, ErrorMessage = "Please enter valid Employee ID")]
+        public int EmployeeId { get; set; }
+    }
+}
