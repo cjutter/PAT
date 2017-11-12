5f02b20cf05552dfe2884abeb5b8a9eb4950f1fb
Charles Utter <cutter@utilant.com>
2017-11-12 18:02:30 -0500

    validation

--- PAT.UI/Areas/Calculations/Controllers/BenefitsCalculationController.cs
+++ PAT.UI/Areas/Calculations/Controllers/BenefitsCalculationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PAT.UI.Areas.Calculations.Models;
 
 namespace PAT.UI.Areas.Calculations.Controllers
 {
@@ -12,6 +13,12 @@ namespace PAT.UI.Areas.Calculations.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult CalculationResult(int employeeId)
+        {
+            var model = new CalculationResultViewModel();
+            return PartialView(model);
         }
     }
 }
---
+++ PAT.UI/Areas/Calculations/Models/CalculationResultViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PAT.UI.Areas.Calculations.Models
+{
+    public class CalculationResultViewModel
+    {
+    }
+}
